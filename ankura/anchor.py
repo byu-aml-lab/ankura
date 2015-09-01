9442d0510ee50f0722c1bb2063ae294257c44a27
Jeffrey Lund <jefflund@gmail.com>
2015-09-01 13:30:59 -0600

    Matrix renames

--- ankura/anchor.py
+++ ankura/anchor.py
@@ -3,7 +3,7 @@
 import numpy
 
 def construct_Q(M):
-    """Constructs a cooccurrence matrix from a sparse docword matrix
+    """Constructs a cooccurrence matrix Q from a sparse docword matrix M
 
     The docword matrix M is expected to be a sparse scipy matrix which can be
     converted to csc form. The output matrix will be a 2d numpy array.
@@ -11,27 +11,27 @@ def construct_Q(M):
     vocab_size, num_docs = M.shape
 
     # See supplementary 4.1 of Aurora et. al. 2012 for information on these
-    tilde_H = M.tocsc()
-    hat_H = numpy.zeros(vocab_size)
+    H_tilde = M.tocsc()
+    H_hat = numpy.zeros(vocab_size)
 
-    # Construct tilde_H and hat_H
-    for j in xrange(tilde_H.indptr.size - 1):
+    # Construct H_tilde and H_hat
+    for j in xrange(H_tilde.indptr.size - 1):
         # get indices of column j
-        col_start = tilde_H.indptr[j]
-        col_end = tilde_H.indptr[j + 1]
-        row_indices = tilde_H.indices[col_start: col_end]
+        col_start = H_tilde.indptr[j]
+        col_end = H_tilde.indptr[j + 1]
+        row_indices = H_tilde.indices[col_start: col_end]
 
         # get count of tokens in column (document) and compute norm
-        count = numpy.sum(tilde_H.data[col_start: col_end])
+        count = numpy.sum(H_tilde.data[col_start: col_end])
         norm = count * (count - 1)
 
-        # update hat_H and tilde_H (see supplementary)
-        hat_H[row_indices] = tilde_H.data[col_start: col_end] / norm
-        tilde_H.data[col_start: col_end] /= numpy.sqrt(norm)
+        # update H_hat and H_tilde (see supplementary)
+        H_hat[row_indices] = H_tilde.data[col_start: col_end] / norm
+        H_tilde.data[col_start: col_end] /= numpy.sqrt(norm)
 
 
     # construct and return normalized Q
-    Q = tilde_H * tilde_H.transpose() - numpy.diag(hat_H)
+    Q = H_tilde * H_tilde.transpose() - numpy.diag(H_hat)
     return numpy.array(Q / num_docs)
 
 
