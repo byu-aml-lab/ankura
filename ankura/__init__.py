d5c7ae95b775105f1dbc086963b8db451b2b2a03
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2015-09-22 17:45:13 -0600

    Added filter for small documents

--- ankura/__init__.py
+++ ankura/__init__.py
@@ -2,6 +2,7 @@
 
 from .pipeline import (read_uci, read_glob,
                        filter_stopwords, filter_rarewords, filter_commonwords,
+                       filter_smalldocs,
                        run_pipeline)
 from .anchor import gramschmidt_anchors, constraint_anchors
 from .topic import recover_topics, predict_topics
