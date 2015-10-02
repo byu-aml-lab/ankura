75f461474458bc9996b59c80c83aafd793c51d61
Jeffrey Lund <jefflund@gmail.com>
2015-10-02 09:26:59 -0600

    Added setup and license

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup for Ankura"""
+
+from setuptools import setup
+
+setup(
+    name='ankura',
+    version='0.1',
+    description='Interactive anchor words implementation',
+    license='MIT License',
+    author='Jeffrey Lund',
+    author_email='jefflund@gmail.com',
+    url='https://github.com/jlund3/ankura',
+    packages=['ankura']
+)
