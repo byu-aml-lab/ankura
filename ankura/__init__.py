3257563ee63b283e166d58b97e04ab4465cee41f
Jeffrey Lund <jefflund@gmail.com>
2016-02-16 09:51:34 -0700

    Moved convert_cooccurrences around

--- ankura/__init__.py
+++ ankura/__init__.py
@@ -1,11 +1,12 @@
 """Ankura provides the ability to experiment with anchor-based topic modeling"""
 
+from .anchor import gramschmidt_anchors, constraint_anchors
 from .pipeline import (read_uci, read_glob, read_file,
                        filter_stopwords, filter_rarewords, filter_commonwords,
                        combine_words,
                        filter_smalldocs,
+                       convert_cooccurences, convert_format,
                        run_pipeline)
-from .anchor import gramschmidt_anchors, constraint_anchors
 from .topic import recover_topics, topic_transform
 
-from . import measure, util
+from . import measure, tokenize, util
