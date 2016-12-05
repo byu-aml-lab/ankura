ee69fa4e28c0aee646325a9509bd8495289c5e7a
Jeffrey Lund <jefflund@gmail.com>
2016-12-05 08:56:42 -0700

    Added topic quality metrics

--- ankura/validate/quality.py
+++ ankura/validate/quality.py
@@ -1,6 +1,7 @@
 """Functions for measuring topic quality"""
 
 import numpy
+import scipy.stats
 
 
 def topic_coherence(word_indices, dataset, epsilon=.01):
@@ -12,3 +13,28 @@ def topic_coherence(word_indices, dataset, epsilon=.01):
             count = dataset.docwords[j].nnz
             coherence += numpy.log((pair_count + epsilon) / count)
     return coherence
+
+
+def w_uniform(topic):
+    """Measures the distance of the topic from the uniform distribution"""
+    V = len(topic)
+    return scipy.stats.entropy(topic, numpy.ones(V) / V)
+
+
+def w_vacuous(topic, dataset):
+    """Measures the distance of the topic from the vacuous distribution"""
+    vacuous = dataset.Q.sum(axis=1) / dataset.Q.sum()
+    return scipy.stats.entropy(topic, vacuous)
+
+
+def d_bground(k, topic_transform):
+    """Measures the distance of the topic from the background distribution"""
+    D = topic_transform.num_docs
+    bground = numpy.ones(D) / D
+
+    topic = topic_transform.M[k, :]
+    topic = numpy.array(topic.todense()).flatten()
+    topic = topic.astype(float)
+    topic /= topic.sum()
+
+    return scipy.stats.entropy(topic, bground)
