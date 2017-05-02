5c1ddba274a82316f540922b82d8e54ac37692e4
Jeffrey Lund <jefflund@gmail.com>
2017-05-02 12:58:25 -0600

    Sped up Q computation with sparse matrices

--- ankura/cooccurrence.py
+++ ankura/cooccurrence.py
@@ -1,5 +1,6 @@
 """Functions for construction and modification of cooccurrence matrices"""
 
+import scipy.sparse
 import numpy
 
 
@@ -7,11 +8,23 @@ def build(corpus):
     """Constructs a cooccurrence matrix from a Corpus"""
     # See supplementary 4.1 of Aurora et al. 2012 for more details
     V, D = len(corpus.vocabulary), len(corpus.documents)
-    Q = numpy.zeros((V, D))
-    for doc in corpus.documents:
-        H_d = numpy.zeros(V)
-        for token in doc.types:
-            H_d[token.type] += 1
+    data, row, col = [], [], []
+    H_hat = numpy.zeros(V)
+
+    for i, doc in enumerate(corpus.documents):
         n_d = len(doc.types)
-        Q += (numpy.outer(H_d, H_d) - numpy.diag(H_d)) / (n_d * (n_d - 1))
-    return Q / D
+        if n_d <= 1:
+            continue
+
+        norm = 1 / (n_d * (n_d - 1))
+        sqrt_norm = numpy.sqrt(norm)
+
+        for token in doc.types:
+            data.append(sqrt_norm)
+            row.append(token.type)
+            col.append(i)
+            H_hat[token.type] += norm
+
+    H_tilde = scipy.sparse.coo_matrix((data, (row, col)), (V, D)).tocsc()
+    Q = H_tilde * H_tilde.T - numpy.diag(H_hat)
+    return numpy.array(Q / D)
