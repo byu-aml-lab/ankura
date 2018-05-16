a9a25c4a48ccfa1896a49b3c0123224d4482439c
wfearn <wilson.fearn@gmail.com>
2018-05-16 09:42:52 -0600

    Added random seed option to pipeline and updated gensim assign

--- ankura/pipeline.py
+++ ankura/pipeline.py
@@ -21,6 +21,7 @@ import pickle
 import re
 import string
 import tarfile
+import time
 
 import bs4
 import scipy.sparse
@@ -613,7 +614,12 @@ def build_docwords(corpus, V=None):
     return docwords.tocsc()
 
 
-def test_train_split(corpus, num_train=None, num_test=None, **kwargs):
+def test_train_split(corpus, num_train=None, num_test=None, random_seed=None, **kwargs):
+
+    if not random_seed:
+        random_seed = time.time()
+    np.random.seed(random_seed)
+
     if not num_train and not num_test:
         num_train = int(len(corpus.documents) * .8)
         num_test = len(corpus.documents) - num_train
